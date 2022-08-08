47d7d0f6606914d46dcae1a0ce97f9db56b9dd33
makansari7878 <75665159+makansari7878@users.noreply.github.com>
2022-08-08 20:06:59 +0530

    menu code

---
+++ login.kt
@@ -0,0 +1,9 @@
+ override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        menuInflater.inflate(R.menu.mymenu, menu)
+    }
